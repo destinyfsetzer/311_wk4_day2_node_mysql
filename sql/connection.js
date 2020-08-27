0e731ddc8724f925edbf24d91907e4996defa758
destinyfsetzer <54375774+destinyfsetzer@users.noreply.github.com>
2020-08-27 09:30:14 -0500

    initial commit

--- sql/connection.js
+++ sql/connection.js
@@ -1,4 +1,5 @@
 const mysql = require('mysql')
+require('dotenv').config()
 
 class Connection {
   constructor() {
@@ -6,10 +7,10 @@ class Connection {
       console.log('creating connection...')
       this.pool = mysql.createPool({
         connectionLimit: 100,
-        host: 'localhost',
-        user: 'root',
-        password: 'password',
-        database: 'admin'
+        host: process.env.HOST,
+        user: process.env.USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DATABASE
       })
 
       return this.pool
